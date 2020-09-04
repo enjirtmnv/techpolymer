47f8dd660f4505b72b2d94fa39a0f0b7afe5bd13
enjirtmnv <shkensinio@gmail.com>
2020-09-04 20:42:36 +0300

    add burger submenu

--- src/js/all-categories.js
+++ src/js/all-categories.js
@@ -5,13 +5,13 @@ $(document).ready(function () {
     $('.categories__item-all-categories').click(function (event) {
         $('.all-categories-home').addClass('show');
         $('.all-categories').addClass('show');
-        $('.categories__wrapper').addClass('hidden');
+        $('.categories__wrapper').addClass('close');
     });
 
     $('.all-categories__nav-item').click(function (event) {
         $('.all-categories-home').removeClass('show');
         $('.all-categories').removeClass('show');
-        $('.categories__wrapper').removeClass('hidden');
+        $('.categories__wrapper').removeClass('close');
     });
 
     // let container = $(".all-categories");
--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     $('.nav__mobile-burger').click(function (event) {
         $('.burger').toggleClass('showBurger');
         $('.header-nav').toggleClass('header-nav-fixed');
-        // $('body').toggleClass('lock');
+        $('body').toggleClass('lock');
         // $('.all-categories').removeClass('show');
         // $('.categories__wrapper').removeClass('hidden');
     });
@@ -19,6 +19,37 @@ $(document).ready(function () {
     //     $('.all-categories').removeClass('show');
     //     $('.categories__wrapper').removeClass('hidden');
     // });
+
+    $('.burger__catalog').click(function (event) {
+        $('.burger-catalog').toggleClass('show');
+        // $('.all-categories').removeClass('show');
+        // $('.categories__wrapper').removeClass('hidden');
+    });
+
+    $('.burger__about').click(function (event) {
+        $('.burger-about').toggleClass('show');
+        // $('.all-categories').removeClass('show');
+        // $('.categories__wrapper').removeClass('hidden');
+    });
+
+    $('.burger__rent').click(function (event) {
+        $('.burger-rent').toggleClass('show');
+        // $('.all-categories').removeClass('show');
+        // $('.categories__wrapper').removeClass('hidden');
+    });
+
+    $('.burger__ransom').click(function (event) {
+        $('.burger-ransom').toggleClass('show');
+        // $('.all-categories').removeClass('show');
+        // $('.categories__wrapper').removeClass('hidden');
+    });
+
+    $('.burger__contacts').click(function (event) {
+        $('.burger-contacts').toggleClass('show');
+        // $('.all-categories').removeClass('show');
+        // $('.categories__wrapper').removeClass('hidden');
+    });
+
 
 });
 
