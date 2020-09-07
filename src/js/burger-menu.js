c9c7558de8d6eceaaf87ffeda551fd10ff69443d
enjirtmnv <shkensinio@gmail.com>
2020-09-07 14:00:18 +0300

    fix burger menu, add menu list

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -9,34 +9,34 @@ $(document).ready(function () {
         $('.list-all-categories').removeClass('show-list-all-categories');
     });
 
-    $('.burger__catalog').on('click',function (event) {
-        $('.burger-catalog').toggleClass('show-sub-menu');
-        $('.burger__nav-arrow').toggleClass('arrow-open');
-        $('.burger-about, .burger-rent, .burger-ransom, .burger-contacts').removeClass('show-sub-menu');
-    });
-
     $('.burger__about').on('click',function (event) {
         $('.burger-about').toggleClass('show-sub-menu');
-        $('.burger__nav-arrow').toggleClass('arrow-open');
+        $('.burger__about-arrow').toggleClass('arrow-open');
         $('.burger-catalog, .burger-rent, .burger-ransom, .burger-contacts').removeClass('show-sub-menu');
     });
 
     $('.burger__rent').on('click',function (event) {
         $('.burger-rent').toggleClass('show-sub-menu');
-        $('.burger__nav-arrow').toggleClass('arrow-open');
+        $('.burger__rent-arrow').toggleClass('arrow-open');
         $('.burger-catalog, .burger-about, .burger-ransom, .burger-contacts').removeClass('show-sub-menu');
     });
 
     $('.burger__ransom').on('click',function (event) {
         $('.burger-ransom').toggleClass('show-sub-menu');
-        $('.burger__nav-arrow').toggleClass('arrow-open');
+        $('.burger__ransom-arrow').toggleClass('arrow-open');
         $('.burger-catalog, .burger-about, .burger-rent, .burger-contacts').removeClass('show-sub-menu');
     });
 
     $('.burger__contacts').on('click',function (event) {
         $('.burger-contacts').toggleClass('show-sub-menu');
-        $('.burger__nav-arrow').toggleClass('arrow-open');
+        $('.burger__contacts-arrow').toggleClass('arrow-open');
         $('.burger-catalog, .burger-about, .burger-rent, .burger-ransom').removeClass('show-sub-menu');
+    });
+
+    $('.burger__catalog').on('click',function (event) {
+        $('.burger-catalog').toggleClass('show-sub-menu');
+        $('.burger__catalog-arrow').toggleClass('arrow-open');
+        $('.burger-about, .burger-rent, .burger-ransom, .burger-contacts').removeClass('show-sub-menu');
     });
 
 
