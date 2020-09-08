afee07a0ba971a8399c8bb5ff0a52b563dc7aed6
enjirtmnv <shkensinio@gmail.com>
2020-09-08 19:49:55 +0300

    fix carousel

--- src/js/scroll-safari.js
+++
@@ -1,7 +0,0 @@
-const preventScrollOnMobile = e => {
-    if (!e.target.closest('.scrollable')) {
-        e.preventDefault();
-    }
-};
-
-document.addEventListener('touchmove', preventScrollOnMobile, { passive: false });
