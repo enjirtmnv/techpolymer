d4084d43c51e37e3ba07d0a06b4ba7c40cfec1fc
enjirtmnv <shkensinio@gmail.com>
2020-09-09 14:32:45 +0300

    fix mouseup list-desk-application

--- src/js/list-desk-application.js
+++ src/js/list-desk-application.js
@@ -4,24 +4,29 @@ $(document).ready(function () {
 
     $('.categories__list-desk-application').click(function (event) {
         if (window.matchMedia('(min-width: 768px)').matches) {
-            // $('.list-desk-application').toggleClass('show-list-desk-application');
             $('.list-desk-application').slideToggle(400);
-
-                // $('.list-desk-application').removeClass('hidden-list-desk-application');
-                // $('.list-desk-application').fadeIn();
-
-
         }
-        if (!$('.categories').hasClass('add-categories-box-shadow')){
+        if (!$('.categories').hasClass('add-categories-box-shadow')) {
             $('.categories').addClass('add-categories-box-shadow');
-        }else{
-
-            setTimeout(function() {
+        } else {
+            setTimeout(function () {
                 $('.categories').removeClass('add-categories-box-shadow');
             }, 399);
-
         }
 
+    });
+
+    $(document).mouseup(e => {
+        if (!$('.list-desk-application').is(e.target)
+            && $('.list-desk-application').has(e.target).length === 0 &&
+            !$('.categories__list-desk-application').is(e.target)
+            && $('.categories__list-desk-application').has(e.target).length === 0
+        ) {
+            $('.list-desk-application').slideUp(400);
+            setTimeout(function () {
+                $('.categories').removeClass('add-categories-box-shadow');
+            }, 399);
+        }
     });
 
 });
