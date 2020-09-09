a61ebaae8450faaa68089377b9c4bccf220a95dd
enjirtmnv <shkensinio@gmail.com>
2020-09-09 14:00:47 +0300

    fix toggleClass list desk application

--- src/js/list-desk-application.js
+++ src/js/list-desk-application.js
@@ -4,10 +4,26 @@ $(document).ready(function () {
 
     $('.categories__list-desk-application').click(function (event) {
         if (window.matchMedia('(min-width: 768px)').matches) {
-            $('.list-desk-application').toggleClass('show-list-desk-application');
+            // $('.list-desk-application').toggleClass('show-list-desk-application');
+            $('.list-desk-application').slideToggle(400);
+
+                // $('.list-desk-application').removeClass('hidden-list-desk-application');
+                // $('.list-desk-application').fadeIn();
+
+
         }
-        $('.categories').toggleClass('add-categories-box-shadow');
+        if (!$('.categories').hasClass('add-categories-box-shadow')){
+            $('.categories').addClass('add-categories-box-shadow');
+        }else{
+
+            setTimeout(function() {
+                $('.categories').removeClass('add-categories-box-shadow');
+            }, 399);
+
+        }
 
     });
 
 });
+
+
