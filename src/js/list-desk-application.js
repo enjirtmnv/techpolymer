78d4e9c65330a4a02c940cf25142730f0bb45190
enjirtmnv <shkensinio@gmail.com>
2020-09-09 12:54:01 +0300

    add down menu desk application

--- src/js/list-desk-application.js
+++ src/js/list-desk-application.js
@@ -2,13 +2,12 @@ import $ from 'jquery';
 
 $(document).ready(function () {
 
-$('.categories__list-desk-application').click(function (event) {
-    $('.list-desk-application').toggleClass('show-list-desk-application');
-    // $('body').addClass('lock');
+    $('.categories__list-desk-application').click(function (event) {
+        if (window.matchMedia('(min-width: 768px)').matches) {
+            $('.list-desk-application').toggleClass('show-list-desk-application');
+        }
+        $('.categories').toggleClass('add-categories-box-shadow');
 
-    // $('.categories__list-desk-application').on('click',function(event){
-    //     $('.list-desk-application').slideToggle();
-    // });
-});
+    });
 
 });
